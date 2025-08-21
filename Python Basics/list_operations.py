5f45e94e519f4a3882ff8917429e58550e3aa565
dithinkrishna45 <dithinkrishna45@gmail.com>
2025-08-21 17:13:51 +0530

    Thu

---
+++ Python Basics/list_operations.py
@@ -0,0 +1,69 @@
+# list1 = ["Apple","Banana","Orange","Mango","Rumbutan"]
+
+# print(list1)
+# print(list1[0])
+# print(list1[1:3])
+
+# list1.append("Milk")
+# list1.append(["Curd","Soap"])
+
+# print(list1)
+
+# if "Milk" in list1:
+#     print ("item found")
+# else :
+#     print("item not found or check the spelling")
+
+
+# while True:
+#     print("******Menu******")
+#     print("\n1.Add\n2.View\n3.Exit")
+#     choice = int(input("Enter Your Choice :"))
+#     if choice == 3:
+#         break
+#     elif choice == 1 :
+#         item = input("Enter a item :")
+
+#         list1.append(item)
+
+#     elif choice == 2 :
+#         for item in list1:
+#             print(item)
+    
+#     else:
+#         print("Invalid Input !!")
+
+
+# for i,it in enumerate(list1,start=1):
+#     print(i,it)
+
+
+grossery = [
+    {"item":"Milk","qty":2,"price":56},
+    {"item":"Egg","qty":12,"price":72},
+    {"item":"Rice","qty":2,"price":80},
+    {"item":"Carrot","qty":1,"price":50},
+    {"item":"Biscut","qty":2,"price":50}
+]
+
+# print(grossery)
+
+while True:
+    print("******Menu*******")
+    print("\n1.Add\n2.View\n3.Exit")
+    choice = int(input("Enter Your Choice :"))
+    if choice == 3:
+        break
+    elif choice == 1 :
+        uitem = input("Enter a item :")
+        uqty = input("Enter a Quantity :")
+        uprice = input("Enter a Price :")
+
+        grossery[item]=item
+
+
+    elif choice == 2 :
+
+    
+    else:
+        print("Invalid Input !!")
